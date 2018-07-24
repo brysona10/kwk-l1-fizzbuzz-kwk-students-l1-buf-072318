464e2d889a1846dc8da9d1833180f673ac2c2f54
brysona10 <addisonbryson10@gmail.com>
2018-07-24 19:02:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # build a program that returns Fizz if #/3, Buzz if #/5 and fizzbuzz if #/3 and #/5 
 
 def fizzbuzz(number)
+  if number %3 ==0 
+    puts "heyyyyy"
   if number %5 == 0 && number %3 == 0
   "FizzBuzz"
   elsif number % 3 == 0 
